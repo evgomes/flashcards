2ebeaf14f017eb1bfd538a88232c77a535e93e91
vimaciel <viniciusfmaciel@gmail.com>
2019-02-17 14:13:27 -0300

    Create layout for new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,17 +1,69 @@
-import React, { Component } from 'react';
-import { Text } from "native-base"
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+import { Container, Content, Footer, FooterTab, Button, Text, Form, Item, Input } from "native-base"
 
 class NewDeck extends Component {
     static navigationOptions = {
         title: 'New Deck',
     }
+
+    state = {
+        title: ''
+    }
+
+    onTitleChange = (e) =>{
+        const title = e.nativeEvent.text
+        this.setState({
+            title
+        })
+    }
+
     render() {
         return (
-            <Text>
-                New Deck
-            </Text>
+            <Container style={styles.container}>
+                <Content>
+                    <Text style={styles.title}>
+                        What is the title of your new deck?
+                    </Text>
+                    <Form style={styles.form}>
+                        <Form>
+                            <Item>
+                                <Input error placeholder="Ex: Javascript" value={this.state.title} onChange={this.onTitleChange} />
+                            </Item>
+                        </Form>
+                    </Form>
+                </Content>
+                <Footer>
+                    <FooterTab>
+                        <Button disabled={this.state.title === ''} full onPress={() => alert(this.state.title)}>
+                            <Text>Add</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
         );
     }
 }
 
-export default NewDeck;
+export default NewDeck
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#D4DBED',
+    },
+
+    form: {
+        backgroundColor: '#FFF',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10
+    },
+
+    title: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginTop: 30,
+        color: '#13244E'
+    }
+})
